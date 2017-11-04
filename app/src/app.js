c71fe326d23c8bb3eaa9ed2d1a0d85ebb3f0e511
Alexandre Monjol <alexandre.monjol@gmail.com>
2017-11-04 15:45:21 +0100

    app > UI fix nav & app wrapper

--- app/src/app.js
+++ app/src/app.js
@@ -37,16 +37,14 @@ export default class App extends React.Component {
           </Nav>
           <Nav className="floatRight">
             <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-              <MenuItem eventKey={3.1}>Action</MenuItem>
-              <MenuItem eventKey={3.2}>Another action</MenuItem>
-              <MenuItem eventKey={3.3}>Something else here</MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey={3.4}>Separated link</MenuItem>
+              <MenuItem eventKey={3.1}>Log out</MenuItem>
             </NavDropdown>
           </Nav>
         </Navbar>
 
-        {this.props.children}
+        <div className="app-wrapper">
+          {this.props.children}
+        </div>
       </div>
     )
   }
