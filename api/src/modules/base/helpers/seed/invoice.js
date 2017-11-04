f5e076832adf6ace4a9b00c572d35c56e9a060c8
Alexandre Monjol <alexandre.monjol@gmail.com>
2017-11-04 18:08:57 +0100

    api > fix invoice seed for customer id

--- api/src/modules/base/helpers/seed/invoice.js
+++ api/src/modules/base/helpers/seed/invoice.js
@@ -7,73 +7,69 @@ module.exports = async function customerSeed(models, accounts) {
     const account = accounts[a];
 
     const customers = JSON.parse(JSON.stringify(await models.Customer.findAll({ where: { accountId: account.id } })))
-    for (let c = 0; c < customers.length; c++) {
-      const customer = customers[c];
-      // Create customer account invoices
-      values.push(
-        {
-          ref: '1',
-          issuedAt: faker.date.past(),
-          dueDate: faker.date.future(),
-          amount: Number(faker.finance.amount()),
-          status: 100,
-          accountId: account.id,
-          customerId: customer.id,
-        }
-      );
 
-      values.push(
-        {
-          ref: '2',
-          issuedAt: faker.date.past(),
-          dueDate: faker.date.past(),
-          amount: Number(faker.finance.amount()),
-          status: 101,
-          paymentDate: faker.date.past(),
-          accountId: account.id,
-          customerId: customer.id,
-        }
-      );
-
-      values.push(
-        {
-          ref: '3',
-          dueDate: faker.date.past(),
-          dueDate: faker.date.past(),
-          amount: Number(faker.finance.amount()),
-          status: 102,
-          accountId: account.id,
-          customerId: customer.id,
-        }
-      );
-
-      values.push(
-        {
-          ref: '3',
-          issuedAt: faker.date.past(),
-          dueDate: faker.date.past(),
-          amount: Number(faker.finance.amount()),
-          status: 102,
-          accountId: account.id,
-          customerId: customer.id,
-        }
-      );
-
-      for (let i = 4; i <= 20; i++) {
-        values.push(
-          {
-            ref: `${i}`,
-            issuedAt: faker.date.recent(),
-            dueDate: faker.date.recent(),
-            amount: Number(faker.finance.amount()),
-            status: faker.random.arrayElement([103, 102, 101, 100]),
-            accountId: account.id,
-            customerId: customer.id,
-          }
-        );
+    values.push(
+      {
+        ref: '1',
+        issuedAt: faker.date.past(),
+        dueDate: faker.date.future(),
+        amount: Number(faker.finance.amount()),
+        status: 100,
+        accountId: account.id,
+        customerId: (faker.random.arrayElement(customers)).id,
       }
+    );
+
+    values.push(
+      {
+        ref: '2',
+        issuedAt: faker.date.past(),
+        dueDate: faker.date.past(),
+        amount: Number(faker.finance.amount()),
+        status: 101,
+        paymentDate: faker.date.past(),
+        accountId: account.id,
+        customerId: (faker.random.arrayElement(customers)).id,
+      }
+    );
+
+    values.push(
+      {
+        ref: '3',
+        dueDate: faker.date.past(),
+        dueDate: faker.date.past(),
+        amount: Number(faker.finance.amount()),
+        status: 102,
+        accountId: account.id,
+        customerId: (faker.random.arrayElement(customers)).id,
+      }
+    );
+
+    values.push(
+      {
+        ref: '4',
+        issuedAt: faker.date.past(),
+        dueDate: faker.date.past(),
+        amount: Number(faker.finance.amount()),
+        status: 102,
+        accountId: account.id,
+        customerId: (faker.random.arrayElement(customers)).id,
+      }
+    );
+
+    for (let i = 5; i <= 20; i++) {
+      values.push(
+        {
+          ref: `${i}`,
+          issuedAt: faker.date.recent(),
+          dueDate: faker.date.recent(),
+          amount: Number(faker.finance.amount()),
+          status: faker.random.arrayElement([103, 102, 101, 100]),
+          accountId: account.id,
+          customerId: (faker.random.arrayElement(customers)).id,
+        }
+      );
     }
   }
-
   return await models.Invoice.bulkCreate(values);
 };
