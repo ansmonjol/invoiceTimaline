d0fa41dde39c573a49e98521f43fc488c3982591
Alexandre Monjol <alexandre.monjol@gmail.com>
2017-11-05 18:21:17 +0100

    api > seeds create multiple accounts

--- api/src/modules/base/helpers/seed/account.js
+++ api/src/modules/base/helpers/seed/account.js
@@ -3,10 +3,16 @@ module.exports = async function accountSeed(models) {
   const values = [];
   const email = faker.internet.email();
 
-  for (let i = 0; i < 1; i++) values.push({
+  values.push({
     email,
     billingEmail: email,
     name: 'eFounders',
+  });
+
+  for (let i = 0; i < 2; i++) values.push({
+    email,
+    billingEmail: email,
+    name: faker.company.companyName(),
   });
   const accounts = await models.Account.bulkCreate(values);
 
